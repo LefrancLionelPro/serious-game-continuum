db0ab8ec864843ba482ff60e8ede16e188b0c92f
LefrancLionelPro <lionel.lefranc.pro@gmail.com>
2026-02-17 10:42:19 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 document.getElementById("scene1").addEventListener("click", function (){
     const hiddenBtnList = document.querySelectorAll('.bouton_cacher');
-    const scene1 = document.querySelector('.scene1');
     if(hiddenBtnList){
         for (let i = 0; i < hiddenBtnList.length; i++) {
             hiddenBtnList[i].style.visibility = "visible";
         }
     }
 
-    scene1.style.pointerEvents = "none";
+    this.style.pointerEvents = "none";
 })
