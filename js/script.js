7f16b24e100825a9530b2a66330aab79c477d3fa
LefrancLionelPro <lionel.lefranc.pro@gmail.com>
2026-02-20 17:51:20 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,13 +1,61 @@
-document.getElementById("scene1").addEventListener("click", function (){
-    const hiddenBtnList = document.querySelectorAll('.bouton_cacher');
+// document.getElementById("scene1").addEventListener("click", function (){
+//     const hiddenBtnList = document.querySelectorAll('.bouton_cacher');
+//
+//     this.pause();
+//     if(hiddenBtnList){
+//         for (let i = 0; i < hiddenBtnList.length; i++) {
+//             hiddenBtnList[i].style.visibility = "visible";
+//         }
+//     }
+//
+//     this.style.pointerEvents = "none";
+// });
 
-    this.pause();
-    if(hiddenBtnList){
-        for (let i = 0; i < hiddenBtnList.length; i++) {
-            hiddenBtnList[i].style.visibility = "visible";
-        }
+const scenario = {
+    "intro" : {
+        videoSrc : "assets/video/Collab.mp4",
+        choix: [
+            { texte: "Option A", cible: "scene_suivante_A"},
+            { texte: "Option B", cible: "scene_suivante_B" },
+        ]
+    },
+    "scene_suivante_A": {
+        videoSrc: "assets/video/popularité.mp4",
+        choix: []
     }
+};
 
-    this.style.pointerEvents = "none";
-    this.pause();
-})
+const containerVid = document.getElementById("conteneur-video");
+function addVideo(url){
+    let newVideo = document.createElement('video');
+    newVideo.src = url;
+    newVideo.autoplay = true;
+    newVideo.muted = true;
+    containerVid.appendChild(newVideo);
+}
+
+const containerBtn = document.getElementById("conteneur-boutons");
+function addBouton(texte, cible){
+    let newBtn = document.createElement('button');
+    newBtn.innerText = texte;
+
+    newBtn.addEventListener('click',function(){
+        chargerScene(cible);
+    });
+    containerBtn.appendChild(newBtn);
+}
+
+function chargerScene(idScene) {
+    const data = scenario[idScene];
+    if (!data) return;
+    containerVid.innerHTML = "";
+    containerBtn.innerHTML = "";
+
+    addVideo(data.videoSrc);
+
+    data.choix.forEach(unChoix => {
+        addBouton(unChoix.texte, unChoix.cible);
+    });
+}
+
+chargerScene("intro");
