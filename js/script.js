e0b2434d69bffa16df6b671deb31805f05fc5d3d
LefrancLionelPro <lionel.lefranc.pro@gmail.com>
2026-02-23 14:04:55 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let trueID;
+let annee;
+let sexe;
+let mdp;
+
 const scenario = {
     "intro" : {
         videoSrc : "assets/video/Collab.mp4",
@@ -53,16 +58,110 @@ function addBouton(texte, cible){
 }
 
 function chargerScene(idScene) {
-    const data = scenario[idScene];
-    if (!data) return;
-    containerVid.innerHTML = "";
+    if (idScene === "identification") {
+        afficherFormulaireID();
+        return;
+    }
+
+    else if (idScene === "login") {
+        return;
+    }
+
+    else {
+        const data = scenario[idScene];
+        if (!data) return;
+        containerVid.innerHTML = "";
+        containerBtn.innerHTML = "";
+
+        addVideo(data.videoSrc);
+
+        data.choix.forEach(unChoix => {
+            addBouton(unChoix.texte, unChoix.cible);
+        });
+    }
+}
+
+function afficherFormulaireID() {
     containerBtn.innerHTML = "";
 
-    addVideo(data.videoSrc);
+    let inputPrenom = document.createElement('input');
+    inputPrenom.placeholder = "Entrez votre Prénom";
+    inputPrenom.id = "inputPrenom";
 
-    data.choix.forEach(unChoix => {
-        addBouton(unChoix.texte, unChoix.cible);
+    let sexes = document.createElement("input");
+    sexes.list = "list";
+
+    let datasexes = document.createElement("datalist");
+    datasexes.id = "list";
+
+    let option1 = document.createElement("option");
+    option1.value = "Homme";
+
+    let option2 = document.createElement("option");
+    option2.value = "Femme";
+
+    let option3 = document.createElement("option");
+    option3.value = "Ne souhaite pas préciser";
+
+
+    let birthYear = document.createElement("input");
+    birthYear.placeholder = "Entrez votre année de naissance";
+    birthYear.type = "text";
+    birthYear.size = 4;
+    birthYear.maxLength = 4;
+    birthYear.type = "number";
+
+    let department = document.createElement("input");
+    department.placeholder = "Entrez le numéro de votre département";
+    department.type = "text";
+    department.size = 2;
+    department.maxLength = 2;
+    department.type = "number";
+
+    let motDePasse = document.createElement("input");
+    motDePasse.type = "password";
+    motDePasse.id = "motDePasse";
+    motDePasse.placeholder = "Veuillez rentrer un mot de passe";
+
+    let btnValider = document.createElement('button');
+    btnValider.innerHTML = "Créer mon ID et commencer";
+
+    containerBtn.appendChild(inputPrenom);
+    containerBtn.appendChild(sexes);
+    containerBtn.appendChild(datasexes);
+
+    datasexes.appendChild(option1);
+    datasexes.appendChild(option2);
+    datasexes.appendChild(option3);
+
+    containerBtn.appendChild(birthYear);
+    containerBtn.appendChild(department);
+    containerBtn.appendChild(motDePasse);
+    containerBtn.appendChild(btnValider);
+
+    btnValider.addEventListener("click", function (){
+        if (inputPrenom.value === "" || birthYear.value === "" || department.value === "" || sexes.value === "" || motDePasse.value === "") {
+            window.alert("veuillez remplir les questions");
+        }
+
+        else {
+            let nameValue = inputPrenom.value;
+            let lettre = nameValue.charAt(0).toUpperCase();
+
+            annee = birthYear.value;
+
+            let dpt = department.value;
+
+            sexe = sexes.value;
+
+            trueID = lettre + annee +dpt;
+
+            mdp = motDePasse.value;
+
+            console.log(trueID);
+            chargerScene("intro")
+        }
     });
 }
 
-chargerScene("intro");
+chargerScene("identification");
